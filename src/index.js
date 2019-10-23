cf4de5cd986075511992af6ccc89305a9ca3402f
artisj <artisj3@gmail.com>
2019-10-23 11:09:26 -0400

    Updated Ican and text
    Added Icons from semantic ui and background color

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./spinner";
 
 // functional based component
 /*
@@ -18,33 +19,51 @@ const App = () => {
 // updating the state will cause the component to rerender
 class App extends React.Component {
   //javascript constructor
+  /*
   constructor(props) {
     //super(props) is required
     super(props);
     // This is the only direct assignment for state
     this.state = { lat: null, errMessage: "" };
+  }
+  */
 
+  // second way of initializing the state
+  // babel will setup the super behind the sceens
+  state = { lat: null, errMessage: "" };
+
+  // called once when the component first loads
+  // Initial data loading should go here
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
     );
   }
 
-  //render function is required
-  render() {
+  // called every time the component is changed it will also
+  // call the render method (state/prop changes)
+  componentDidUpdate() {
+    console.log("My component was just updated");
+  }
+
+  // render helper to avoid conditions in the render function
+  renderContent() {
     if (this.state.errMessage && !this.state.lat) {
       return <div>Error: {this.state.errMessage}</div>;
     }
 
     if (!this.state.errMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please Allow Location Request" />;
+  }
+  // render function is required
+  // used to render jsx
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
